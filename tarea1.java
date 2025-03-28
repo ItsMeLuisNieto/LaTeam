0d3e5e546c211257d347c20e14da3ffa50feceeb
Luis Angel Nieto Paz <U24222429@utp.edu.pe>
2025-03-28 09:49:18 -0500

    Update tarea1.java

--- tarea1.java
+++ tarea1.java
@@ -2,3 +2,16 @@
 
 import java.util.Scanner;
 
+public class area() {
+  public static void main(String[] args) {
+    int geom_fig;
+    bool confirm_exit = false;
+    double result;
+    double base, height;
+    while (confirm_exit = false) {
+      System.out.println("<---- Calculadora de área ---->")
+      System.out.println("Selecciona 1 para el área del círculo")
+      System.out.println("Selecciona 2 para el área del triángulo")
+    }
+  }
+}
